ca0ef1fc44f3e8b0ad6ba9c4c9d4e3bf15a4b725
Bolu Akinsola <boluakins@gmail.com>
2022-03-23 13:00:37 +0100

    add redis cache

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -32,6 +32,11 @@ namespace Basket.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
             });
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration["ConnectionStrings:Redis"];
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
